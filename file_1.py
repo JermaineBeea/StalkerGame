40534d3e6585f5c027375c852f4cce5bda75d9e4
Jermaine Beea <jermainebeea@gmail.com>
2024-09-29 21:17:05 +0200

    file_1 added

---
+++ file_1.py
@@ -0,0 +1,62 @@
+import tkinter
+
+
+def positionRoot (root, width_ratio = 1/2, height_ratio = 1/2, x_driftRatio = 0, y_driftRatio = 0):
+	""" 
+		This function places modifies the widget width and lenght relative to screen width and height. 
+		And modifies the position on screen relative to the center of the screen
+		
+		Args: 
+			width_ratio(float: default 1/2): The width(x-axis) of the root widget as a fraction of the screen width.
+			height_ratio(float: default 1/2): The height(y-axis) of the root widget as a fraction of the screen height.
+			x_driftRatio(float: default 0): The x axis deviation(+ for 'right', - for 'left') from center, represented as a fraction of the maximum x-axis deviation(screen width//2)
+			y_driftRatio(float: default 0): The y axis deviation(+ for 'down', - for 'up') from center, represented as a fraction of the maximum y-axis absolute deviation(screen width//2)
+		
+		Returns:
+			Function has no return, as it modifies root in place
+
+	"""
+	
+	screen_width = root.winfo_screenwidth()
+	screen_height = root.winfo_screenheight()
+
+	screen_centerX = screen_width//2	
+	screen_centerY = screen_height//2
+	
+	root_width = int(screen_width*width_ratio)
+	root_height = int(screen_height*height_ratio)
+	
+	root_centerX = root_width//2
+	root_centerY = root_height//2
+
+	screen_centerX = int(screen_centerX*(1 + x_driftRatio))
+	screen_centerY = int(screen_centerY*(1 + y_driftRatio))
+	
+	root_x_change = screen_centerX - root_centerX
+	root_y_change = screen_centerY - root_centerY
+
+	root.geometry(f'{root_width}x{root_height}+{root_x_change}+{root_y_change}')
+
+
+
+tk_root = tkinter.Tk()
+tk_root.title('Main Root')
+positionRoot(tk_root, 1/2, 1/2)
+
+
+tk_root.mainloop()
+
+	
+	
+	
+
+
+
+
+
+
+
+
+
+	
+	
