e7c07a5e7213dd41be7ea5cbe1a25d1650f67f32
wtc <wtc@pop-os.localdomain>
2024-10-08 16:42:41 +0200

    modified

--- randButton.py
+++ randButton.py
@@ -2,42 +2,40 @@ import tkinter
 import time
 from Template.Button import positionRoot 
 
-def run_main ():
-  # count = 0
-  def buttonFunc (event = None):
-    # global count
-    # count += 1
-    # button.config(bg = 'red')
-    # if count == 3: button.config(bg = 'green')
-    run_main()
+def buttonFunc (event = None):
+  global count
+  count += 1
+  button.config(bg = 'red')
+  if count == 3: button.config(bg = 'green')
 
-  tk_instance = tkinter.Tk()
-  tk_instance.title('Main Root')
-  tk_instance.config(bg='black')
 
-  x_ratio = 1 / 2
-  y_ratio = 1 / 2
+tk_instance = tkinter.Tk()
+tk_instance.title('Main Root')
+tk_instance.config(bg='black')
 
-  positionRoot(tk_instance, width_ratio=x_ratio, height_ratio=y_ratio)
+x_ratio = 1 / 2
+y_ratio = 1 / 2
 
-  # Button initial relative positions to center of widget, and size parameters
-  x_0 = 0.5
-  y_0 = 0.5
-  button_width = 1
-  button_height = 1
+positionRoot(tk_instance, width_ratio=x_ratio, height_ratio=y_ratio)
 
-  button = tkinter.Button(tk_instance, command = buttonFunc , bg = 'white', width = button_width, height = button_height, 
-  borderwidth = 2, highlightthickness = 0, activebackground= 'grey', relief = 'raised')
-  # relif can also equal: flat, sunken, groove, ridge
-  button.place(relx = x_0, rely = y_0, anchor = 'center')
-      
+# Button initial relative positions to center of widget, and size parameters
+x_0 = 0.5
+y_0 = 0.5
+button_width = 1
+button_height = 1
 
-  end_time = time.perf_counter()
+button = tkinter.Button(tk_instance, command = buttonFunc , bg = 'white', width = button_width, height = button_height, 
+borderwidth = 2, highlightthickness = 0, activebackground= 'grey', relief = 'raised')
+# relif can also equal: flat, sunken, groove, ridge
+button.place(relx = x_0, rely = y_0, anchor = 'center')
+    
 
-  print(end_time)
+end_time = time.perf_counter()
 
-  tk_instance.mainloop()
+print(end_time)
 
-run_main()
+tk_instance.mainloop()
+
+
 
 
